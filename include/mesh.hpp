e60b0cef07e0180b346c9c2dd171c173fca11b82
jack <JACKJBARRETT@PM.ME>
2026-03-28 12:04:09 -0700

    feat: obj_loader is done and seems to be working... however, there is an issue with (1) the way I'm doing floating point comparisons in mesh.hpp (comparing floats for equality is a rookie mistake) and (2) I'm not happy with how this solution would scale to large meshes. so, this commit is really a checkpoint. I plan to re-implement using an associative array

--- include/mesh.hpp
+++ include/mesh.hpp
@@ -8,9 +8,15 @@ typedef struct {
     float u, v;         // texture coord
 } Vertex;
 
+inline bool operator==(const Vertex& a, const Vertex& b) {
+    return a.px == b.px && a.py == b.py && a.pz == b.pz
+        && a.nx == b.nx && a.ny == b.ny && a.nz == b.nz
+        && a.u  == b.u  && a.v  == b.v;
+}
+
 typedef struct {
     Vertex* vertices;    // Vec3 pos, Vec3 normal, Vec2 uv — interleaved
     uint32_t* indices;    // every 3 = one triangle
-    uint32_t vertex_count;
-    uint32_t index_count;
+    uint32_t vertex_count = 0;
+    uint32_t index_count = 0;
 } Mesh;
--- src/obj_loader.cpp
+++ src/obj_loader.cpp
@@ -45,6 +45,7 @@
 //
 
 #include "../include/obj_loader.hpp"
+#include <cstdint>
 #include <fstream>
 #include <glm/ext/vector_float3.hpp>
 #include <glm/fwd.hpp>
@@ -53,6 +54,7 @@
 #include <vector>
 #include <glm/glm.hpp>
 #include <iostream>
+#include <cassert>
 
 typedef struct {
     glm::uint32_t v1;
@@ -78,6 +80,15 @@ void process_face(std::vector<face_data_indices>& faces, std::istringstream& ss)
         std::istringstream bs(blob);
         bs >> vs[i] >> slash >> vts[i] >> slash >> vns[i];
         ++i;
+    }
+
+    // Face indices into vertex, normal, and texture coordinate lists assume
+    // 1-based indexing in .obj files. Here, we map indicies i to i-1 for 0-based
+    // indexing to simplify later processing.
+    for (uint32_t i = 0; i < 3; ++i) {
+        --vs[i];
+        --vts[i];
+        --vns[i];
     }
 
     face.v1 = vs[0]; face.v2 = vs[1]; face.v3 = vs[2];
@@ -136,5 +147,74 @@ Mesh load_obj(const std::string& filename) {
     // that indexes into this array. Each 3 consecutive indices in the latter array
     // correspond to a face of the mesh.
 
-    return Mesh{};
+    // TODO(jack): this is silly. We should do something smarter re: determining
+    // the number of vertices and indices we'll ultimately need.
+    Mesh mesh;
+    uint32_t num_faces = faces.size();
+    mesh.vertices = new Vertex[num_faces * 3];
+    mesh.indices = new glm::uint32_t[num_faces * 3];
+
+    // pseudocode:
+    // for each Vertex that would be implied by the indices in our face array
+    //     create vertices v1, v2, v3
+    //     create indices i, j, k
+    //     for each Vertex in mesh.vertices
+    //          if we find a Vertex equal to vi anywhere in the array then don't do anything to mesh.vertices
+    //          and the corresponding index goes at the back of mesh.indices
+    //
+    //          else the vertex vi is unique and new so: add it to the back of mesh.vertices and the corresponding index
+    //          goes at the back of mesh.indices
+    //
+    //          for the above two conditions, take care to increment vertex_count and index_count accordingly
+    //
+    // a postcondition: should have index_count/3 == number_faces
+    auto populate_vertex_data = [&vertices, &normals, &texture_coords]
+                                (Vertex& v, const uint32_t v_idx, const uint32_t vn_idx, const uint32_t vt_idx) {
+                                    glm::vec3 v_pos = vertices[v_idx];
+                                    v.px = v_pos.x;
+                                    v.py = v_pos.y;
+                                    v.pz = v_pos.z;
+
+                                    glm::vec3 v_normal = normals[vn_idx];
+                                    v.nx = v_normal.x;
+                                    v.ny = v_normal.y;
+                                    v.nz = v_normal.z;
+
+                                    glm::vec2 v_texture = texture_coords[vt_idx];
+                                    v.u = v_texture.x;
+                                    v.v = v_texture.y;
+                                };
+
+    auto add_to_mesh = [&mesh](const Vertex& vertex) {
+        for (uint32_t i = 0; i < mesh.vertex_count; ++i) {
+            if (mesh.vertices[i] == vertex) {
+                // since we FOUND a copy of vertex we're trying to insert in the mesh,
+                // we need only specify its index in the indices array.
+                mesh.indices[mesh.index_count++] = i;
+                return;
+            }
+        }
+
+        // if we get here, then we did not find a vertex in the mesh identical
+        // to the vertex we are trying to add. this means that we need to add it
+        // to our mesh's vertex array and increment the sizes of the arrays accordingly
+        uint32_t& mesh_vertices_ct = mesh.vertex_count;
+        mesh.vertices[mesh_vertices_ct] = vertex;
+        mesh.indices[mesh.index_count++] = mesh_vertices_ct++;
+    };
+
+    for (uint32_t i = 0; i < num_faces; ++i) {
+        Vertex v1, v2, v3;
+        face_data_indices fdi = faces[i];
+        populate_vertex_data(v1, fdi.v1, fdi.vn1, fdi.vt1);
+        populate_vertex_data(v2, fdi.v2, fdi.vn2, fdi.vt2);
+        populate_vertex_data(v3, fdi.v3, fdi.vn3, fdi.vt3);
+        add_to_mesh(v1);
+        add_to_mesh(v2);
+        add_to_mesh(v3);
+    }
+
+    assert(((mesh.index_count / 3) == num_faces) && "Incorrect number of faces. Something is wrong.");
+
+    return mesh;
 }
